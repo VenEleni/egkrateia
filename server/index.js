1389f0893cb5ac9229c97f4b891d742ef5f90121
Eleni Veniou Nikolidaki <venelenii@gmail.com>
2024-07-13 13:02:55 +0300

    navbar name

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
